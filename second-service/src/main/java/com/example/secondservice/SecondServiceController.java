f104a810485261f4ad77241d3cefa1fee8856caa
MunSooKim <anstn1993@gmail.com>
2021-08-30 12:23:42 +0900

    feat : spring cloud gateway를 이용하여 api gateway 서버 생성
    - 필터를 이용하여 api gateway에서 요청과 응답 전에 필요한 작업을 할 수 있게 처리
    
    - eureka discovery service와 연동하여 마이크로 서비스의 검색 및 로드밸런싱 처리

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,12 +1,30 @@
 package com.example.secondservice;
 
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/second-service")
+@Slf4j
 public class SecondServiceController {
   @GetMapping("/welcome")
   public String welcome() {
     return "Welcome to the Second Service";
+  }
+
+  @GetMapping("/message")
+  public String message(@RequestHeader("second-request") String header) {
+    log.info(header);
+    return "Hello world in Second Service.";
+  }
+
+  @GetMapping("/check")
+  public String check(HttpServletRequest request) {
+    log.info("Server port={}", request.getServerPort());
+    return "Hi, there. This is a message Second Service";
   }
 }
